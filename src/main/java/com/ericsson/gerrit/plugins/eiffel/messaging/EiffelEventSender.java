4afcae0356cc11860ec629bba3fae12ad867fb34
Christoffer Cortes Sjöwall <christoffer.cortes.sjowall@ericsson.com>
2019-10-15 12:41:59 +0200

    Add header

--- src/main/java/com/ericsson/gerrit/plugins/eiffel/messaging/EiffelEventSender.java
+++ src/main/java/com/ericsson/gerrit/plugins/eiffel/messaging/EiffelEventSender.java
@@ -1,3 +1,19 @@
+/*
+   Copyright 2019 Ericsson AB.
+   For a full list of individual contributors, please see the commit history.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 package com.ericsson.gerrit.plugins.eiffel.messaging;
 
 import java.io.IOException;
@@ -106,6 +122,7 @@ public class EiffelEventSender {
         final String password = pluginConfig.getRemremPassword();
         final String url = pluginConfig.getRemremPublishURL();
 
+        // eiffel-commons:1.0.1 not available on jitpack
         //httpRequest.setBasicAuth(username, password);
         String auth = String.format("%s:%s", username, password);
         String encodedAuth = new String(Base64.encodeBase64(auth.getBytes()), "UTF-8");
