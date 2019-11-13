a6a16519c71d8223db724ab4bd7b34f9668baf18
Mattias Linnèr <mattias.linner@ericsson.com>
2019-11-13 15:56:29 +0100

    Improved gerrit mock

--- src/functionaltests/java/com/ericsson/gerrit/plugins/eiffel/linking/GerritMock.java
+++ src/functionaltests/java/com/ericsson/gerrit/plugins/eiffel/linking/GerritMock.java
@@ -1,32 +1,82 @@
 package com.ericsson.gerrit.plugins.eiffel.linking;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public class GerritMock {
 
-    private GitCommit head;
+    private Map<String, GitCommit> branches;
+    private Map<String, String> changeIds;
+    private Map<String, GitCommit> commits;
+    private Map<String, ChangeInfo> changeInfos;
 
     public GerritMock() {
-        head = getNewCommit();
+        branches = new HashMap<String, GitCommit>();
+        changeIds = new HashMap<String, String>();
+        commits = new HashMap<String, GitCommit>();
+        changeInfos = new HashMap<String, ChangeInfo>();
     }
 
-    public GitCommit newCommit() {
-        return getNewCommit(head);
+    public void createBranch(String branch) {
+        branches.put(branch, getNewCommit());
     }
 
-    public GitCommit getCommit() {
-        return head;
+    public GitCommit getHead(String branch) {
+        return branches.get(branch);
     }
 
-    public GitCommit merge(GitCommit update) {
-        assert update.parentSha.equals(head.sha);
-        head = update;
-        return update;
+    public GitCommit getCommit(String changeId) {
+        return commits.get(changeId);
     }
 
-    public GitCommit rebase(GitCommit update) {
-        return getNewCommit(head);
+    public String createNewChange(String user, String branch) {
+        String lookupKey = getLookupKey(user, branch);
+        String changeId = "change-id-" + changeIds.size();
 
+        assert !changeIds.containsKey(lookupKey);
+        changeIds.put(lookupKey, changeId);
+        changeInfos.put(changeId, new ChangeInfo(user, branch));
+
+        GitCommit branchHead = getHead(branch);
+        GitCommit changeCommit = getNewCommit(branchHead);
+        commits.put(changeId, changeCommit);
+        return changeId;
+    }
+
+    public GitCommit createNewPatchSet(String changeId) {
+        GitCommit oldCommit = commits.get(changeId);
+        GitCommit newCommit = getNewCommit();
+
+        // As we have not rebased the change the startig point has not changed.
+        newCommit.parentSha = oldCommit.parentSha;
+        commits.put(changeId, newCommit);
+        return newCommit;
+    }
+
+    public String getChangeId(String user, String branch) {
+        String lookupKey = getLookupKey(user, branch);
+        return changeIds.get(lookupKey);
+    }
+
+    public GitCommit rebase(String changeId) {
+        String branch = changeInfos.get(changeId).branch;
+        GitCommit head = getHead(branch);
+        GitCommit rebased = getNewCommit(head);
+        commits.put(changeId, rebased);
+        return rebased;
+
+    }
+
+    public GitCommit submit(String changeId) {
+        GitCommit submittedCommit = getCommit(changeId);
+        String branch = changeInfos.get(changeId).branch;
+
+        // Ensure its rebased
+        assert submittedCommit.parentSha.equals(getHead(branch).sha);
+
+        branches.put(branch, submittedCommit);
+        return submittedCommit;
     }
 
     private GitCommit getNewCommit() {
@@ -39,6 +89,21 @@ public class GerritMock {
         GitCommit commit = getNewCommit();
         commit.parentSha = parentCommit.sha;
         return commit;
+    }
+
+
+    private String getLookupKey(String user, String branch) {
+        return user + branch;
+    }
+
+    class ChangeInfo {
+        String branch;
+        String user;
+
+        public ChangeInfo(String user, String branch) {
+            this.user = user;
+            this.branch = branch;
+        }
     }
 
 }
--- src/functionaltests/java/com/ericsson/gerrit/plugins/eiffel/linking/GerritMockTest.java
+++ src/functionaltests/java/com/ericsson/gerrit/plugins/eiffel/linking/GerritMockTest.java
@@ -7,26 +7,96 @@ import org.junit.Test;
 public class GerritMockTest {
 
     @Test
-    public void test() {
+    public void oneUser() {
         GerritMock gerritMock = new GerritMock();
-        GitCommit base = gerritMock.getCommit();
-        GitCommit update1 = gerritMock.newCommit();
-        GitCommit update2 = gerritMock.newCommit();
-        assertEquals(base.sha, update1.parentSha);
-        assertEquals(base.sha, update2.parentSha);
+        String branch = "branch";
+        String user1 = "user1";
+        gerritMock.createBranch(branch);
+        GitCommit base = gerritMock.getHead(branch);
 
-        GitCommit update2Merged = gerritMock.merge(update2);
-        assertEquals(base.sha, update2Merged.parentSha);
-        assertEquals(update2Merged.sha, gerritMock.getCommit().sha);
+        String changeId = gerritMock.createNewChange(user1, branch);
+        GitCommit update = gerritMock.getCommit(changeId);
+        assertEquals(base.sha, update.parentSha);
 
-        GitCommit update1Rebased = gerritMock.rebase(update1);
-        assertEquals(update2Merged.sha, update1Rebased.parentSha);
+        GitCommit newPatchSet = gerritMock.createNewPatchSet(changeId);
+        assertEquals(base.sha, newPatchSet.parentSha);
 
-        GitCommit update1Merged = gerritMock.merge(update1Rebased);
-        assertEquals(update2Merged.sha, update1Merged.parentSha);
-        assertEquals(update1Merged.sha, gerritMock.getCommit().sha);
-
-
+        GitCommit merged = gerritMock.submit(changeId);
+        assertEquals(base.sha, merged.parentSha);
+        assertEquals(merged.sha, gerritMock.getHead(branch).sha);
     }
 
+    @Test
+    public void oneUserTwoBranches() {
+        GerritMock gerritMock = new GerritMock();
+        String branch1 = "branch1";
+        String branch2 = "branch2";
+        String user1 = "user1";
+        gerritMock.createBranch(branch1);
+        gerritMock.createBranch(branch2);
+        GitCommit base = gerritMock.getHead(branch1);
+        GitCommit base2 = gerritMock.getHead(branch2);
+
+        String changeId = gerritMock.createNewChange(user1, branch1);
+        assertEquals(changeId, gerritMock.getChangeId(user1, branch1));
+
+        GitCommit update = gerritMock.getCommit(changeId);
+        assertEquals(base.sha, update.parentSha);
+
+        String changeId2 = gerritMock.createNewChange(user1, branch2);
+        assertEquals(changeId2, gerritMock.getChangeId(user1, branch2));
+        
+        GitCommit update2 = gerritMock.getCommit(changeId2);
+        assertEquals(base2.sha, update2.parentSha);
+
+        GitCommit newPatchSet = gerritMock.createNewPatchSet(changeId);
+        assertEquals(base.sha, newPatchSet.parentSha);
+
+        GitCommit newPatchSet2 = gerritMock.createNewPatchSet(changeId2);
+        assertEquals(base2.sha, newPatchSet2.parentSha);
+
+        GitCommit merged = gerritMock.submit(changeId);
+        assertEquals(base.sha, merged.parentSha);
+        assertEquals(merged.sha, gerritMock.getHead(branch1).sha);
+
+        GitCommit merged2 = gerritMock.submit(changeId2);
+        assertEquals(base2.sha, merged2.parentSha);
+        assertEquals(merged2.sha, gerritMock.getHead(branch2).sha);
+    }
+
+    @Test
+    public void twoUsers() {
+        GerritMock gerritMock = new GerritMock();
+        String branch = "branch1";
+        String user1 = "user1";
+        String user2 = "user2";
+        gerritMock.createBranch(branch);
+        GitCommit base = gerritMock.getHead(branch);
+
+        String changeId = gerritMock.createNewChange(user1, branch);
+        GitCommit update = gerritMock.getCommit(changeId);
+        assertEquals(base.sha, update.parentSha);
+
+        String changeId2 = gerritMock.createNewChange(user2, branch);
+        assertEquals(changeId2, gerritMock.getChangeId(user2, branch));
+
+        
+        GitCommit update2 = gerritMock.getCommit(changeId2);
+        assertEquals(base.sha, update2.parentSha);
+
+        GitCommit merged2 = gerritMock.submit(changeId2);
+        assertEquals(base.sha, merged2.parentSha);
+        assertEquals(merged2.sha, gerritMock.getHead(branch).sha);
+
+        GitCommit newPatchSet = gerritMock.createNewPatchSet(changeId);
+        assertEquals(base.sha, newPatchSet.parentSha);
+        
+        GitCommit rebase = gerritMock.rebase(changeId);
+        assertEquals(rebase.parentSha, merged2.sha);
+
+        GitCommit merged = gerritMock.submit(changeId);
+        assertEquals(merged2.sha, merged.parentSha);
+        assertEquals(merged.sha, gerritMock.getHead(branch).sha);
+
+    }
 }
