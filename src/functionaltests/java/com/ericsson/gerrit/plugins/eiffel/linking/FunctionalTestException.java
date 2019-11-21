e2f7fcd608f17298c4a4f66e8ac320fd8cc63f68
Mattias Linner <mattias.linner@ericsson.com>
2019-11-21 12:13:53 +0100

    Fix codacy issue

--- src/functionaltests/java/com/ericsson/gerrit/plugins/eiffel/linking/FunctionalTestException.java
+++ src/functionaltests/java/com/ericsson/gerrit/plugins/eiffel/linking/FunctionalTestException.java
@@ -1,11 +1,10 @@
 package com.ericsson.gerrit.plugins.eiffel.linking;
 
 public class FunctionalTestException extends RuntimeException {
+    private static final long serialVersionUID = -3685895201014211455L;
 
     public FunctionalTestException(String message, Throwable cause) {
         super(message, cause);
     }
-
-    private static final long serialVersionUID = -3685895201014211455L;
 
 }
