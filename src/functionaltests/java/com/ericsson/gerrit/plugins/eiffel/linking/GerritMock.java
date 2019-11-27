8d44d792f27b0ce1f452031b177bc22b4c2236f5
Mattias Linner <mattias.linner@ericsson.com>
2019-11-27 14:48:11 +0100

    Add javadoc

--- src/functionaltests/java/com/ericsson/gerrit/plugins/eiffel/linking/GerritMock.java
+++ src/functionaltests/java/com/ericsson/gerrit/plugins/eiffel/linking/GerritMock.java
@@ -11,6 +11,9 @@ import java.util.UUID;
 import com.ericsson.gerrit.plugins.eiffel.git.CommitInformation;
 import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
 
+/**
+ * This class will very simply simulate Gerrit when creating new changes and submitting them.
+ */
 public class GerritMock {
 
     private Map<String, GitCommit> branches;
@@ -25,19 +28,51 @@ public class GerritMock {
         changeInfos = new HashMap<String, ChangeInfo>();
     }
 
+    /**
+     * Initializes a branch with an commit
+     *
+     * @param branch a branch name to use during test
+     */
     public void createBranch(String branch) {
         branches.put(branch, getNewCommit());
     }
 
+    /**
+     * Get the "HEAD" commit from a branch
+     *
+     * @param branch name of branch to get commit for
+     * @return the commit the branch points to
+     */
     public GitCommit getHead(String branch) {
         assert branches.containsKey(branch);
         return branches.get(branch);
     }
 
+    /**
+     * Fetches the current commit for a changeId
+     *
+     * @param changeId the change id to fetch the commit for
+     * @return the current commit of the changeId
+     */
     public GitCommit getCommit(String changeId) {
         return commits.get(changeId);
     }
 
+    /**
+     * Simulates creating a new branch in Gerrit. Will generate a changeId and create a new commit
+     * based on HEAD for the given branch. The commit will have the HEAD commit as parent.
+     *
+     * As the function requires both <code>user</code> and <code>branch</code> many scenarios can be
+     * tested:
+     * <ul>
+     * <li>Many users on the same branch</li>
+     * <li>One user working on many branches</li>
+     * </ul>
+     *
+     * @param user   Name of the user to create the change for
+     * @param branch Name of branch this will
+     * @return a changeId
+     */
     public String createNewChange(String user, String branch) {
         String lookupKey = getLookupKey(user, branch);
         String changeId = "change-id-" + changeIdsLookup.size();
@@ -56,6 +91,13 @@ public class GerritMock {
         return changeId;
     }
 
+    /**
+     * Simulates creating a new patchset on the given change. The method will create a new commit
+     * with the same parent as before.
+     *
+     * @param changeId The changeId for the change to be updated
+     * @return the new commit
+     */
     public GitCommit createNewPatchSet(String changeId) {
         GitCommit oldCommit = commits.get(changeId);
         GitCommit newCommit = getNewCommit();
@@ -66,11 +108,25 @@ public class GerritMock {
         return newCommit;
     }
 
+    /**
+     * Returns the changeId for the given user and branch
+     *
+     * @param user   Name of user
+     * @param branch Name of branch
+     * @return the changeId
+     */
     public String getChangeId(String user, String branch) {
         String lookupKey = getLookupKey(user, branch);
         return changeIdsLookup.get(lookupKey);
     }
 
+    /**
+     * Simulates rebasing a change in Gerrit. It will create a new commit with branch HEAD as parent
+     * and set that commit as current commit for this change.
+     *
+     * @param changeId the changeId of the change
+     * @return the new commit
+     */
     public GitCommit rebase(String changeId) {
         String branch = changeInfos.get(changeId).branch;
         GitCommit head = getHead(branch);
@@ -80,6 +136,14 @@ public class GerritMock {
 
     }
 
+    /**
+     * Simulates submitting a change in Gerrit. Will update branch head to the change commit and
+     * then remove the connection of user and branch to the change. The test implementation only
+     * supports one change per user and branch.
+     *
+     * @param changeId
+     * @return
+     */
     public GitCommit submit(String changeId) {
         GitCommit submittedCommit = getCommit(changeId);
         String branch = changeInfos.get(changeId).branch;
@@ -96,6 +160,17 @@ public class GerritMock {
         return submittedCommit;
     }
 
+    /**
+     * Will set an expectation on the given CommitInformation to check for the correct commit and
+     * project. The expectation will then return the parent SHA for the asked commit.
+     *
+     * @param commitInformation The CommitInformation to set expectation on
+     * @param changeId          the current change containing commit SHA and parent SHA used in the
+     *                          expectation
+     * @param projectName       project name used in the expectation
+     * @throws ResourceNotFoundException
+     * @throws IOException
+     */
     public void setExpectionsFor(CommitInformation commitInformation, String changeId,
             String projectName)
             throws ResourceNotFoundException, IOException {
@@ -120,7 +195,7 @@ public class GerritMock {
         return user + branch;
     }
 
-    class ChangeInfo {
+    private class ChangeInfo {
         public String branch;
         public String user;
     }
