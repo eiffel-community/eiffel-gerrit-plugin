78d15fadc2e3a4db5846c67504e99f3a12fde9b7
Mattias Linner <mattias.linner@ericsson.com>
2019-11-29 12:30:27 +0100

    Use constants in test

--- src/functionaltests/java/com/ericsson/gerrit/plugins/eiffel/linking/GerritMockTest.java
+++ src/functionaltests/java/com/ericsson/gerrit/plugins/eiffel/linking/GerritMockTest.java
@@ -12,15 +12,18 @@ import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
 
 public class GerritMockTest {
 
+    private static final String USER_1 = "user1";
+    private static final String USER_2 = "user2";
+    private static final String BRANCH_1 = "branch1";
+    private static final String BRANCH_2 = "branch2";
+
     @Test
     public void oneUser() {
         final GerritMock gerritMock = new GerritMock();
-        final String branch = "branch";
-        final String user1 = "user1";
-        gerritMock.createBranch(branch);
-        final GitCommit base = gerritMock.getHead(branch);
+        gerritMock.createBranch(BRANCH_1);
+        final GitCommit base = gerritMock.getHead(BRANCH_1);
 
-        final String changeId = gerritMock.createNewChange(user1, branch);
+        final String changeId = gerritMock.createNewChange(USER_1, BRANCH_1);
         final GitCommit update = gerritMock.getCommit(changeId);
         assertEquals("Not correct parrent", base.sha, update.parentSha);
 
@@ -31,30 +34,27 @@ public class GerritMockTest {
         assertEquals("The submitted commit does not have correct parent", base.sha,
                 merged.parentSha);
         assertEquals("The branch is not updated correctly", merged.sha,
-                gerritMock.getHead(branch).sha);
+                gerritMock.getHead(BRANCH_1).sha);
     }
 
     @Test
     public void oneUserTwoBranches() {
         final GerritMock gerritMock = new GerritMock();
-        final String branch1 = "branch1";
-        final String branch2 = "branch2";
-        final String user1 = "user1";
-        gerritMock.createBranch(branch1);
-        gerritMock.createBranch(branch2);
-        final GitCommit base = gerritMock.getHead(branch1);
-        final GitCommit base2 = gerritMock.getHead(branch2);
+        gerritMock.createBranch(BRANCH_1);
+        gerritMock.createBranch(BRANCH_2);
+        final GitCommit base = gerritMock.getHead(BRANCH_1);
+        final GitCommit base2 = gerritMock.getHead(BRANCH_2);
 
-        final String changeId = gerritMock.createNewChange(user1, branch1);
+        final String changeId = gerritMock.createNewChange(USER_1, BRANCH_1);
         assertEquals("Lookup did not return correct changId", changeId,
-                gerritMock.getChangeId(user1, branch1));
+                gerritMock.getChangeId(USER_1, BRANCH_1));
 
         final GitCommit update = gerritMock.getCommit(changeId);
         assertEquals("Not correct parrent", base.sha, update.parentSha);
 
-        final String changeId2 = gerritMock.createNewChange(user1, branch2);
+        final String changeId2 = gerritMock.createNewChange(USER_1, BRANCH_2);
         assertEquals("Lookup did not return correct changId", changeId2,
-                gerritMock.getChangeId(user1, branch2));
+                gerritMock.getChangeId(USER_1, BRANCH_2));
 
         final GitCommit update2 = gerritMock.getCommit(changeId2);
         assertEquals("Not correct parrent", base2.sha, update2.parentSha);
@@ -70,31 +70,28 @@ public class GerritMockTest {
         assertEquals("The submitted commit does not have correct parent", base.sha,
                 merged.parentSha);
         assertEquals("The branch is not updated correctly", merged.sha,
-                gerritMock.getHead(branch1).sha);
+                gerritMock.getHead(BRANCH_1).sha);
 
         final GitCommit merged2 = gerritMock.submit(changeId2);
         assertEquals("The submitted commit does not have correct parent", base2.sha,
                 merged2.parentSha);
         assertEquals("The branch is not updated correctly", merged2.sha,
-                gerritMock.getHead(branch2).sha);
+                gerritMock.getHead(BRANCH_2).sha);
     }
 
     @Test
     public void twoUsers() {
         final GerritMock gerritMock = new GerritMock();
-        final String branch = "branch1";
-        final String user1 = "user1";
-        final String user2 = "user2";
-        gerritMock.createBranch(branch);
-        final GitCommit base = gerritMock.getHead(branch);
+        gerritMock.createBranch(BRANCH_1);
+        final GitCommit base = gerritMock.getHead(BRANCH_1);
 
-        final String changeId = gerritMock.createNewChange(user1, branch);
+        final String changeId = gerritMock.createNewChange(USER_1, BRANCH_1);
         final GitCommit update = gerritMock.getCommit(changeId);
         assertEquals("Not correct parrent", base.sha, update.parentSha);
 
-        final String changeId2 = gerritMock.createNewChange(user2, branch);
+        final String changeId2 = gerritMock.createNewChange(USER_2, BRANCH_1);
         assertEquals("Lookup did not return correct changId", changeId2,
-                gerritMock.getChangeId(user2, branch));
+                gerritMock.getChangeId(USER_2, BRANCH_1));
 
         final GitCommit update2 = gerritMock.getCommit(changeId2);
         assertEquals("Not correct parrent", base.sha, update2.parentSha);
@@ -103,7 +100,7 @@ public class GerritMockTest {
         assertEquals("The submitted commit does not have correct parent", base.sha,
                 merged2.parentSha);
         assertEquals("The branch is not updated correctly", merged2.sha,
-                gerritMock.getHead(branch).sha);
+                gerritMock.getHead(BRANCH_1).sha);
 
         final GitCommit newPatchSet = gerritMock.createNewPatchSet(changeId);
         assertEquals("Parent changed when creating new patchset", base.sha, newPatchSet.parentSha);
@@ -115,7 +112,7 @@ public class GerritMockTest {
         assertEquals("The submitted commit does not have correct parent", merged2.sha,
                 merged.parentSha);
         assertEquals("The branch is not updated correctly", merged.sha,
-                gerritMock.getHead(branch).sha);
+                gerritMock.getHead(BRANCH_1).sha);
     }
 
     /**
@@ -125,11 +122,10 @@ public class GerritMockTest {
     public void createWithoutSubmitFail() {
         final GerritMock gerritMock = new GerritMock();
 
-        final String branch = "branch";
-        gerritMock.createBranch(branch);
+        gerritMock.createBranch(BRANCH_1);
 
-        gerritMock.createNewChange("user", branch);
-        gerritMock.createNewChange("user", branch);
+        gerritMock.createNewChange(USER_1, BRANCH_1);
+        gerritMock.createNewChange(USER_1, BRANCH_1);
     }
 
     /**
@@ -139,11 +135,10 @@ public class GerritMockTest {
     public void submitBeforeRebaseFail() {
         final GerritMock gerritMock = new GerritMock();
 
-        final String branch = "branch";
-        gerritMock.createBranch(branch);
+        gerritMock.createBranch(BRANCH_1);
 
-        final String changeUser1 = gerritMock.createNewChange("user1", branch);
-        final String changeUser2 = gerritMock.createNewChange("user2", branch);
+        final String changeUser1 = gerritMock.createNewChange(USER_1, BRANCH_1);
+        final String changeUser2 = gerritMock.createNewChange(USER_2, BRANCH_1);
         gerritMock.submit(changeUser2);
         gerritMock.submit(changeUser1);
     }
@@ -152,17 +147,16 @@ public class GerritMockTest {
     public void createTwoChages() {
         final GerritMock gerritMock = new GerritMock();
 
-        final String branch = "branch";
-        gerritMock.createBranch(branch);
+        gerritMock.createBranch(BRANCH_1);
 
         final String user = "user";
-        final String change1 = gerritMock.createNewChange(user, branch);
+        final String change1 = gerritMock.createNewChange(user, BRANCH_1);
         assertEquals("Lookup did not return correct changId", change1,
-                gerritMock.getChangeId(user, branch));
+                gerritMock.getChangeId(user, BRANCH_1));
         gerritMock.submit(change1);
-        final String change2 = gerritMock.createNewChange(user, branch);
+        final String change2 = gerritMock.createNewChange(user, BRANCH_1);
         assertEquals("Lookup did not return correct changId", change2,
-                gerritMock.getChangeId(user, branch));
+                gerritMock.getChangeId(user, BRANCH_1));
 
     }
 
@@ -170,9 +164,8 @@ public class GerritMockTest {
     public void setCollectionsExpectation() throws ResourceNotFoundException, IOException {
 
         final GerritMock gerritMock = new GerritMock();
-        final String branch = "branch";
-        gerritMock.createBranch(branch);
-        final String changeId = gerritMock.createNewChange("user", branch);
+        gerritMock.createBranch(BRANCH_1);
+        final String changeId = gerritMock.createNewChange(USER_1, BRANCH_1);
         final GitCommit commit = gerritMock.getCommit(changeId);
 
         final CommitInformation commitInformation = mock(CommitInformation.class);
